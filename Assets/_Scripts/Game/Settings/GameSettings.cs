fb92fe25cb58d60bf4d5b7a0abc4fdd61af0c26f
Levi Visser <Levi@rebels.io>
2023-03-08 10:34:41 +0100

    Added team scriptable objects

--- Assets/_Scripts/Game/Settings/GameSettings.cs
+++ Assets/_Scripts/Game/Settings/GameSettings.cs
@@ -1,7 +1,17 @@
-﻿namespace _Scripts.Game.Settings
+﻿using System;
+using System.Collections.Generic;
+using _Scripts.Game.MatchGroups;
+
+namespace _Scripts.Game.Settings
 {
     public class GameSettings : BaseSettings
     {
-        
+        public TeamSettings TeamSettings;
+    }
+
+    [Serializable]
+    public class TeamSettings
+    {
+        public List<Team> Teams = new();
     }
 }
---
+++ Assets/_Scripts/Game/Teams/Team.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace _Scripts.Game.MatchGroups
+{
+    [CreateAssetMenu(fileName = "Team", menuName = "ScriptableObjects/TeamObject", order = 1)]
+    public class Team : ScriptableObject
+    {
+        public string Name;
+        public int Strength;
+    }
+}
